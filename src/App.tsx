b93448d43b64962502bc3d47d14b6133dab7c042
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-10-28 07:48:49 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1>ReplayStudio GUI</h1>
+      <p>Welcome to the Minecraft replay editor.</p>
+    </div>
+  );
+}
+
+export default App;
